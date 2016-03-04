add64bcd9a85fc7ba1b4665001aa8d68b94e60a1
kumar <qservices.sandeep@gmail.com>
2016-03-04 18:35:45 +0530

    connected to live server

--- login.js
+++ login.js
@@ -13,7 +13,7 @@
     
     // attempt to establish a connection to the server
     try {
-        var server = io.connect('http://127.0.0.1:8080');
+        var server = io.connect('http://tempchat.herokuapp.com');
     }
     catch(e) {
         alert('Sorry, we couldn\'t connect. Please try again later \n\n' + e);
